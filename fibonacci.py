6532540b2b2ec028442f70829981e15bf57494af
Asad Ali Khan <48471395+asad8389@users.noreply.github.com>
2020-07-26 23:03:38 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,12 @@
 # Uses python3
 def naive_fib(n):
-    """slow execution due to multiple recursive calls"""
+    """slow execution due to multiple recursive calls, takes alot of time even for n=45"""
     if (n <= 1):
         return n
+    return naive_fib(n - 1) + naive_fib(n - 2)
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
 def calc_fib(n):
-    """fast and better algo for fibonacci series"""
+    """fast and better algo for fibonacci series, runs smoothly even for n>10000"""
     if n<=1:
         return n
     fib = [0]*n
